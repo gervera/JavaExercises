5fb78bac7349b570b7dd706c1bf5ccbc19060dce
gervera <47795468+gervera@users.noreply.github.com>
2021-01-27 12:03:00 -0700

    StringBuilder
    Se cambio de String a StringBuilder la concatenacion del string.

--- ReverseString.java
+++ ReverseString.java
@@ -1,15 +1,16 @@
 
 package ReverseLettersInString;
 import java.lang.StringBuilder;
+import java.util.Arrays;
 
 public class ReverseString {
 
   
     public static void main(String[] args) {
-        String str = "5J%.A3$%b13n-Ks";
+        String str = "3Aa323# %&!a4 B%$ 4C2";
         System.out.println("String Inicial: " + str);
         str = reverseString(str);
-        System.out.println("String Final:   " + str);
+        System.out.println("\nString Final:   " + str);
     }
     
     public static String reverseString(String string){
@@ -22,22 +23,24 @@ public class ReverseString {
             }
         }
         int letras = st.length()-1;
-        String nuevoString = "";
+        StringBuilder nuevoString = new StringBuilder("");
+        System.out.print(st.toString()+ " => ");
         
         for(int i=0;i<string.length();i++){
             char caracter = string.toUpperCase().charAt(i);
             if(isLetter(caracter)){
-                nuevoString=nuevoString + st.charAt(letras);
+                System.out.print(st.charAt(letras));
+                nuevoString.append(st.charAt(letras));
                 letras--;
             }else{
-                nuevoString= nuevoString + string.charAt(i);
+                nuevoString.append(string.charAt(i));
             }
         } 
-        return nuevoString;
+        return nuevoString.toString();
     }
     
     public static boolean isLetter(char c){
-        if(c < 65 || c > 90){
+        if(c < 65 || c > 90 ){
             return false;
         }
         return true;
